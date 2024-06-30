31e2af96ac3ded3a5e96e2b78c9782dc96f7e5ad
Hosein <taremin.hosein@hati.co>
2024-06-30 14:36:12 +0330

    feat: add hint to text

--- src/components/input/text/index.tsx
+++ src/components/input/text/index.tsx
@@ -25,91 +25,95 @@ function TextInput(
     errorMessage,
     LeftIconClass,
     background = "bg-neutral-900",
+    hint,
     ...other
   }: TextInputType,
   ref: any,
 ) {
   return (
-    <Container className="relative">
-      <Container className="relative  float-label-input bg-inherit	">
-        {Icon && (
-          <Container center className="absolute inset-y-0 right-0 w-8">
-            <Icon />
-          </Container>
-        )}
-        {LeftIcon === undefined && stickyText && (
-          <Container center className="absolute inset-y-0 left-4">
-            <Text>{stickyText}</Text>
-          </Container>
-        )}
-        {LeftIcon && (
-          <Container
-            center
-            className={cn(
-              "absolute left-4 -translate-y-2/4 top-2/4",
-              LeftIconClass,
-            )}
-          >
-            <LeftIcon />
-          </Container>
-        )}
-        <input
-          placeholder=" "
-          className={cn(
-            "focus:outline-none focus:shadow-outline focus:border-primary py-3 px-3  appearance-none leading-normal  block w-full rounded-lg border border-neutral-500  bg-gray-50 p-2.5 text-sm text-white outline-none",
-            size === SIZE_ENUM.XXS && "h-4",
-            size === SIZE_ENUM.XS && "h-6",
-            size === SIZE_ENUM.SM && "h-7",
-            size === SIZE_ENUM.MD && "h-8",
-            size === SIZE_ENUM.LG && "h-10",
-            size === SIZE_ENUM.XL && "h-12",
-            size === SIZE_ENUM.XXL && "h-14",
-            background,
-            Icon && "pr-8",
-            stickyText && "pl-8",
-            errorMessage &&
-              "border-danger focus:border-danger-500 border-solid focus:ring-0",
-            className,
+    <Container>
+      <Container className="relative">
+        <Container className="relative  float-label-input bg-inherit	">
+          {Icon && (
+            <Container center className="absolute inset-y-0 right-0 w-8">
+              <Icon />
+            </Container>
           )}
-          ref={ref}
-          value={value && value}
-          type={type}
-          id={id}
-          {...other}
-        />
-
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        {label && (
-          <label
-            htmlFor={id}
+          {LeftIcon === undefined && stickyText && (
+            <Container center className="absolute inset-y-0 left-4">
+              <Text>{stickyText}</Text>
+            </Container>
+          )}
+          {LeftIcon && (
+            <Container
+              center
+              className={cn(
+                "absolute left-4 -translate-y-2/4 top-2/4",
+                LeftIconClass,
+              )}
+            >
+              <LeftIcon />
+            </Container>
+          )}
+          <input
+            placeholder=" "
             className={cn(
-              "absolute right-3 -translate-y-2/4 top-2/4 pointer-events-none transition duration-200 ease-in-out py-0 px-2 text-grey-darker block  text-white",
-              size === SIZE_ENUM.XXS && "text-xs",
-              size === SIZE_ENUM.XS && "text-xs",
-              size === SIZE_ENUM.SM && "text-xs",
-              size === SIZE_ENUM.MD && "text-xs",
-              size === SIZE_ENUM.LG && "text-sm",
-              size === SIZE_ENUM.XL && "text-base",
-              size === SIZE_ENUM.XXL && "text-lg",
+              "focus:outline-none focus:shadow-outline focus:border-primary py-3 px-3  appearance-none leading-normal  block w-full rounded-lg border border-neutral-500  bg-gray-50 p-2.5 text-sm text-white outline-none",
+              size === SIZE_ENUM.XXS && "h-4",
+              size === SIZE_ENUM.XS && "h-6",
+              size === SIZE_ENUM.SM && "h-7",
+              size === SIZE_ENUM.MD && "h-8",
+              size === SIZE_ENUM.LG && "h-10",
+              size === SIZE_ENUM.XL && "h-12",
+              size === SIZE_ENUM.XXL && "h-14",
               background,
-              labelClassName,
-              errorMessage && "text-danger",
+              Icon && "pr-8",
+              stickyText && "pl-8",
+              errorMessage &&
+                "border-danger focus:border-danger-500 border-solid focus:ring-0",
+              className,
             )}
-            style={Icon && { fontSize: "80%", top: 0 }}
+            ref={ref}
+            value={value && value}
+            type={type}
+            id={id}
+            {...other}
+          />
+
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          {label && (
+            <label
+              htmlFor={id}
+              className={cn(
+                "absolute right-3 -translate-y-2/4 top-2/4 pointer-events-none transition duration-200 ease-in-out py-0 px-2 text-grey-darker block  text-white",
+                size === SIZE_ENUM.XXS && "text-xs",
+                size === SIZE_ENUM.XS && "text-xs",
+                size === SIZE_ENUM.SM && "text-xs",
+                size === SIZE_ENUM.MD && "text-xs",
+                size === SIZE_ENUM.LG && "text-sm",
+                size === SIZE_ENUM.XL && "text-base",
+                size === SIZE_ENUM.XXL && "text-lg",
+                background,
+                labelClassName,
+                errorMessage && "text-danger",
+              )}
+              style={Icon && { fontSize: "80%", top: 0 }}
+            >
+              {label}
+            </label>
+          )}
+        </Container>
+        {errorMessage && (
+          <Text
+            className="px-2 absolute -bottom-7"
+            size={SIZE_ENUM.MD}
+            color={COLOR_ENUM.ERROR}
           >
-            {label}
-          </label>
+            {errorMessage}
+          </Text>
         )}
       </Container>
-      {errorMessage && (
-        <Text
-          className="px-2 absolute -bottom-7"
-          size={SIZE_ENUM.MD}
-          color={COLOR_ENUM.ERROR}
-        >
-          {errorMessage}
-        </Text>
-      )}
+      {hint && <Text color={COLOR_ENUM.XLIGHT_GRAY}>{hint}</Text>}
     </Container>
   );
 }
