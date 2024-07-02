2540587e1660113de6f35784d19ea985d5f8dabf
Hosein <taremin.hosein@hati.co>
2024-07-02 15:22:29 +0330

    fix: :lipstick: fix style for hint

--- src/components/input/text/index.tsx
+++ src/components/input/text/index.tsx
@@ -105,7 +105,7 @@ function TextInput(
         </Container>
         {errorMessage && (
           <Text
-            className="px-2 absolute -bottom-7"
+            className="px-2 absolute -bottom-8"
             size={SIZE_ENUM.MD}
             color={COLOR_ENUM.ERROR}
           >
@@ -114,7 +114,7 @@ function TextInput(
         )}
         {!errorMessage && hint && (
           <Text
-            className="px-2 absolute -bottom-7"
+            className="px-2 absolute -bottom-8"
             color={COLOR_ENUM.XLIGHT_GRAY}
             size={SIZE_ENUM.MD}
           >
