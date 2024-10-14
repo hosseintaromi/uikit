7f2877c83d8efc637d19d39422f4f0a2b1906b68
hosseintaromi <hosseintaromii@gmail.com>
2024-10-14 11:50:50 +0330

    feat: :zap: last

---
+++ src/contexts/index.tsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+export const themeUiKit = createContext<any>({
+  isLight: true,
+});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./components";
 export * from "./@types/index";
+export { themeUiKit } from "./contexts";
 // export * from "./hooks";
